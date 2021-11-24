58b98cb900b3d0449a978a7b77889d22b3bc1440
Gulshan-J <94396238+Gulshan-J@users.noreply.github.com>
2021-11-24 19:07:43 +0530

    Update test.c

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -16,32 +16,18 @@ void tearDown()
 
 }
 
-void test_successfullWin()
+void odd_or_even()
 {
-    char array[17]={'0','X','X','X','X',
-                        'X','O','O','O',
-                        'X','X','X','O',
-                        'O','O','O','X'};
-     TEST_ASSERT_EQUAL(1, check_if_win(array));
+     int x=10;
+     int n=0;
+     TEST_ASSERT_EQUAL("Invalid", odd_or_even(n));
+     TEST_ASSERT_EQUAL("Invalid", odd_or_even(x));
 }
-void test_successfuldraw()
-{
-    char array[17]={'0',  'X','O','X','X',
-                        'X','O','O','O',
-                        'X','X','X','O',
-                        'O','O','O','X'};
-    TEST_ASSERT_EQUAL(0,check_if_win(array));
-}
-void test_Emptyboard()
-{
-    char array[17]={ '\0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' , 'j', 'k', 'l', 'm', 'n', 'y', 'z'};
-    TEST_ASSERT_EQUAL(-1,check_if_win(array));
-}
+
 int main()
 {
   UNITY_BEGIN();
-  RUN_TEST(test_successfullWin);
-  RUN_TEST(test_successfuldraw);
-  RUN_TEST(test_Emptyboard);
+  RUN_TEST(odd_or_even);
+ 
   return UNITY_END();
 }
