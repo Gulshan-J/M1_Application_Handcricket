0c05f20cb13f5836ed183b68fa2929178013bc03
Gulshan-J <94396238+Gulshan-J@users.noreply.github.com>
2021-11-24 18:41:00 +0530

    Update function.h

--- 3_Implementation/irc/function.h
+++ 3_Implementation/irc/function.h
@@ -1,9 +1,27 @@
 #ifndef __FUNCTION_H__
 #define __FUNCTION_H__
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 
-int check_if_win(char *);
-int tic_tac_toe(char *);
-int board(char *);
-#endif
+
+void display_bat(int,int);
+void display_bowl(int ,int);
+int user_bat2(int );
+int user_bowl2(int);
+int com_bat();
+int com_bowl();
+int com_user_bat(int);
+int com_user_bowl(int);
+int user_bat();
+int user_bowl();
+int toss(int,int);
+  
+
+  
+  
+  
+  
+  
+  #endif
