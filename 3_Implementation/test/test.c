531f8d3ae6b3737be22740d17698ff2d912768d2
Gulshan-J <94396238+Gulshan-J@users.noreply.github.com>
2021-11-24 18:45:44 +0530

    Create test.c

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,47 @@
+
+#include "unity.h"
+#include "function.h"
+
+
+#define PROJECT_NAME    "HANDCRICKET"
+
+
+void setUp()
+{   
+
+}
+
+void tearDown()
+{
+
+}
+
+void test_successfullWin()
+{
+    char array[17]={'0','X','X','X','X',
+                        'X','O','O','O',
+                        'X','X','X','O',
+                        'O','O','O','X'};
+     TEST_ASSERT_EQUAL(1, check_if_win(array));
+}
+void test_successfuldraw()
+{
+    char array[17]={'0',  'X','O','X','X',
+                        'X','O','O','O',
+                        'X','X','X','O',
+                        'O','O','O','X'};
+    TEST_ASSERT_EQUAL(0,check_if_win(array));
+}
+void test_Emptyboard()
+{
+    char array[17]={ '\0', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i' , 'j', 'k', 'l', 'm', 'n', 'y', 'z'};
+    TEST_ASSERT_EQUAL(-1,check_if_win(array));
+}
+int main()
+{
+  UNITY_BEGIN();
+  RUN_TEST(test_successfullWin);
+  RUN_TEST(test_successfuldraw);
+  RUN_TEST(test_Emptyboard);
+  return UNITY_END();
+}
